fddbe182ec2d08aa5e223d96ec47bd32d091a4d9
Evrei-XD <rom_ser2011@mail.ru>
2023-05-08 16:31:32 +0300

    проект готов к интеграции unity-приложения

--- app/src/main/java/com/example/appforunityintegration2/MainActivity.kt
+++ app/src/main/java/com/example/appforunityintegration2/MainActivity.kt
@@ -2,10 +2,30 @@ package com.example.appforunityintegration2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.appforunityintegration2.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private var threadFlag = false
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+//        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater).also { setContentView (it.root) }
+
+        binding.startThreadBtn.setOnClickListener {
+            threadFlag = !threadFlag
+            startTread()
+        }
+    }
+
+    private fun startTread() {
+        Thread {
+            while (threadFlag) {
+                System.err.println("Tread")
+                //TODO делать что-то из библиотеки
+                Thread.sleep(1000)
+            }
+        }.start()
     }
 }
