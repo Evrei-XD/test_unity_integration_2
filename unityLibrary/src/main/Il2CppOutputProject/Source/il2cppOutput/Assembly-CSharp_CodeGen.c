a2ca773668079edd5089a34067898a4d67c3527b
Evrei-XD <rom_ser2011@mail.ru>
2023-05-12 19:07:19 +0300

    Добавлен серсис, генерирующий широковещательный интент передачи данных, которые принимает бродкаст ресивер в библиотеке-плагине и передает данные Unity-приложению

---
+++ unityLibrary/src/main/Il2CppOutputProject/Source/il2cppOutput/Assembly-CSharp_CodeGen.c
@@ -0,0 +1,77 @@
+﻿#include "pch-c.h"
+#ifndef _MSC_VER
+# include <alloca.h>
+#else
+# include <malloc.h>
+#endif
+
+
+#include "codegen/il2cpp-codegen-metadata.h"
+
+
+
+
+
+extern void AndroidPluginCallback__ctor_m53E818FA1EF41F2D9CFFE98BFFE53EE0558FEEFF (void);
+extern void AndroidPluginCallback_onPositive_m9C6951A7E6DF7B91E3E106F686CBA3E167DB0176 (void);
+extern void AndroidPluginCallback_onNegative_m557A54BE07BE0C78FE0BF5741E4397C313465444 (void);
+extern void PluginActivity_Start_m1879C2A0E6F3EA403EFF318EAC67453EB6504C49 (void);
+extern void PluginActivity_Update_mBACFC1B8428F31FC62CF524B2FECF1EA212E088F (void);
+extern void PluginActivity_Add_m8C2D85AB8223FE495B4C0E4541A15CB2E256B444 (void);
+extern void PluginActivity_ShowToast_m16A64E4D0D88C5E07D3494A2AAF2FFA602442267 (void);
+extern void PluginActivity_GetInput_mA974F147F9B6B1E0ED15B58FE18A0573AD676BCC (void);
+extern void PluginActivity_ResultTrue_m6819E630F8D9DF73D076EC9D3CCC1EA37F4A3583 (void);
+extern void PluginActivity__ctor_m8E205185987DFAD53F5DF7B1857590B41AD8EE4C (void);
+extern void UnitySourceGeneratedAssemblyMonoScriptTypes_v1_Get_mBEB95BEB954BB63E9710BBC7AD5E78C4CB0A0033 (void);
+extern void UnitySourceGeneratedAssemblyMonoScriptTypes_v1__ctor_mE70FB23ACC1EA12ABC948AA22C2E78B2D0AA39B1 (void);
+static Il2CppMethodPointer s_methodPointers[12] = 
+{
+	AndroidPluginCallback__ctor_m53E818FA1EF41F2D9CFFE98BFFE53EE0558FEEFF,
+	AndroidPluginCallback_onPositive_m9C6951A7E6DF7B91E3E106F686CBA3E167DB0176,
+	AndroidPluginCallback_onNegative_m557A54BE07BE0C78FE0BF5741E4397C313465444,
+	PluginActivity_Start_m1879C2A0E6F3EA403EFF318EAC67453EB6504C49,
+	PluginActivity_Update_mBACFC1B8428F31FC62CF524B2FECF1EA212E088F,
+	PluginActivity_Add_m8C2D85AB8223FE495B4C0E4541A15CB2E256B444,
+	PluginActivity_ShowToast_m16A64E4D0D88C5E07D3494A2AAF2FFA602442267,
+	PluginActivity_GetInput_mA974F147F9B6B1E0ED15B58FE18A0573AD676BCC,
+	PluginActivity_ResultTrue_m6819E630F8D9DF73D076EC9D3CCC1EA37F4A3583,
+	PluginActivity__ctor_m8E205185987DFAD53F5DF7B1857590B41AD8EE4C,
+	UnitySourceGeneratedAssemblyMonoScriptTypes_v1_Get_mBEB95BEB954BB63E9710BBC7AD5E78C4CB0A0033,
+	UnitySourceGeneratedAssemblyMonoScriptTypes_v1__ctor_mE70FB23ACC1EA12ABC948AA22C2E78B2D0AA39B1,
+};
+static const int32_t s_InvokerIndices[12] = 
+{
+	4336,
+	3533,
+	3533,
+	4336,
+	4336,
+	4336,
+	4336,
+	3533,
+	3533,
+	4336,
+	6314,
+	4336,
+};
+IL2CPP_EXTERN_C const Il2CppCodeGenModule g_AssemblyU2DCSharp_CodeGenModule;
+const Il2CppCodeGenModule g_AssemblyU2DCSharp_CodeGenModule = 
+{
+	"Assembly-CSharp.dll",
+	12,
+	s_methodPointers,
+	0,
+	NULL,
+	s_InvokerIndices,
+	0,
+	NULL,
+	0,
+	NULL,
+	0,
+	NULL,
+	NULL,
+	NULL,
+	NULL,
+	NULL,
+	NULL,
+};
